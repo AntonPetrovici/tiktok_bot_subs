76063589012f1b28ae6fc69dad90e0a8c1bc2c3e
greedy informal <vodonog9@gmail.com>
2023-03-19 14:25:46 +0300

    tiktok_tg_bot v1.7

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ python.exe -m pip install --upgrade pip
 py -m pip install wheel setuptools pip --upgrade
 pip install pyTelegramBotAPI
 pip install DonationAlertsAPI
-python main.py
+python main.pyw
 pause
