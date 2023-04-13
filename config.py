4a8902b08f3671c0ef47127abe764755d88b9206
greedy informal <75523665+AntonPetrovici@users.noreply.github.com>
2023-04-13 20:06:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-telebot_token = "6041950939:AAHWpZPss04UI07CS-MQ8vENsu9AjsIWRuo"
-da_alert_widget_token = 'Us0Q7Z69fREDrIxizs31'
-donate_url = "https://www.donationalerts.com/r/greedy_informal"
-arhive_link = "https://t.me/+VzcrQ24FCAA1NTIy"
+telebot_token = ""
+da_alert_widget_token = ''
+donate_url = ""
+arhive_link = ""
